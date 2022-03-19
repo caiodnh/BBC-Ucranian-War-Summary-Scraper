96a4c1debfcfbc01a72f76c74a98a20f6360cbe3
Seno <caiodnh@protonmail.com>
2022-03-19 12:24:24 -0230

    also put hour at summary's header

--- scraper.py
+++ scraper.py
@@ -1,25 +1,25 @@
 import requests
-from datetime import date
-from typing import List
+from datetime import datetime
+from typing import Iterable
 
 from bs4 import BeautifulSoup
 
-def get_points() -> List[str]:
+def get_points() -> Iterable[str]:
   url = "https://www.bbc.com/news/live/world-europe-60802572"
   page = requests.get(url)
 
   soup = BeautifulSoup(page.content, 'html5lib')
   items = soup.find_all("li", class_ = "lx-c-summary-points__item")
-  return [item.contents[0] for item in items]
+  return (item.contents[0] for item in items)
 
-def flatten_points(points : List[str]) -> str:
+def flatten_points(points : Iterable[str]) -> str:
   marked_points = map(lambda x: "- " + x, points)
 
   text = "\n\n".join(marked_points)
   return text
 
 def add_header(text : str) -> str:
-  header = "BBC's Summary - " + date.today().strftime("%B %d, %Y")
+  header = datetime.now().strftime("BBC's Summary - %B %d, %Y at %H:%M:%S")
   final = header + "\n```\n" + text + "\n```"
   return final
 
