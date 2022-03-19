93bfa461afeb5ad6541dd2ccc14f12656d52e53c
Seno <caiodnh@protonmail.com>
2022-03-19 12:00:50 -0230

    use BeautifulSoup instead of regex

--- scraper.py
+++ scraper.py
@@ -1,21 +1,22 @@
 import requests
-import re
 from datetime import date
 from typing import List
+
+from bs4 import BeautifulSoup
 
 def get_points() -> List[str]:
   url = "https://www.bbc.com/news/live/world-europe-60802572"
   page = requests.get(url)
 
-  exp = re.compile(r"class=\"lx-c-summary-points__item\".*?>(.*?)<")
-  return exp.findall(page.text)
+  soup = BeautifulSoup(page.content, 'html5lib')
+  items = soup.find_all("li", class_ = "lx-c-summary-points__item")
+  return [item.contents[0] for item in items]
 
 def flatten_points(points : List[str]) -> str:
   marked_points = map(lambda x: "- " + x, points)
 
   text = "\n\n".join(marked_points)
-  corrected_apostrophes = text.replace("&#x27;", "'")
-  return corrected_apostrophes
+  return text
 
 def add_header(text : str) -> str:
   header = "BBC's Summary - " + date.today().strftime("%B %d, %Y")
