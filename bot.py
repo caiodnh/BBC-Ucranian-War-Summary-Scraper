87b94e4a40c09e730b4bd1f7e7d9b3e8d03fb5e5
Seno <caiodnh@protonmail.com>
2022-03-22 00:49:59 -0230

    bot now uses ctx.respond if it can

--- bot.py
+++ bot.py
@@ -7,14 +7,13 @@ bot = discord.Bot()
 async def on_ready():
   print('We have logged in as {0.user}'.format(bot))
 
-# @bot.event
-# async def on_message(message):
-#   if message.content.startswith('$bbc_summary'):
-#     await message.channel.send(get_summary())
-
 @bot.slash_command(guild_ids = [954851709421760522,694962879102976070], description = "Fetch BBC's summary about the war on Ukraine")
 async def bbc_summary(ctx):
-  await ctx.send(get_summary())
+  post = get_summary()
+  try:
+    await ctx.respond(post) # raises an exception if it takes more than 3s
+  except:
+    await ctx.send(post)
 
 with open("token") as file:
   bot.run(file.readline())
