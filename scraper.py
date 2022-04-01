8ac49584e34c5da1c5c14d4f2d8f68feea91b238
Seno <caiodnh@protonmail.com>
2022-04-01 00:02:55 -0230

    New version, works only with BBC

--- scraper.py
+++ scraper.py
@@ -1,63 +1,59 @@
-from typing import Any, Iterable, Tuple
+from typing import Any, Callable, Iterable, Tuple
+import re
 import requests
 from bs4 import BeautifulSoup
 
-def read_url() -> str:
-  # the last line in the file corresponds to the most recent webpage
-  with open('saved_urls.txt', 'r') as file:
-    for line in file:
-      url = line
-  return url
+class LiveNews:
+  def __init__(self,
+              main_url : str,
+              liveblog_url_pattern : str,
+              get_points : Callable[[str], Iterable[str]],
+              header : str):
+    self.main_url = main_url
+    self.liveblog_url_pattern = liveblog_url_pattern
+    self.get_points = get_points
+    self.header = header
 
-def get_page(url : str) -> Any: # returns the page as BeatifulSoup object
+def bbc_get_points(url : str) -> Iterable[str]:
   page = requests.get(url)
   soup = BeautifulSoup(page.content, "html5lib")
-  return soup
-
-def find_last_post(soup : Any) -> Any:
-  post = soup.find("li", class_ = "lx-stream__post-container placeholder-animation-finished")
-  header = post.find("header").find("span").contents[0]
-  header = header.lower()
-  if "live" in header and "moving" in header:
-    return post
-  else:
-    return None
-
-def update_page(url : str, soup : Any) -> Tuple[str, Any]:
-  # check if there is a new page and, in this case, updade both the file and soup object
-
-  last_post = find_last_post(soup)
-
-  if last_post:
-    link = last_post.find("a")
-    url = link["href"]
-    soup = get_page(url)
-
-    # save url as the last line in the file
-    with open('saved_urls.txt', 'a') as file:
-      file.write("\n" + url)
-      
-    url, soup = update_page(url, soup)
-
-  return url, soup
-
-def get_points(soup : Any) -> Iterable[str]:
   items = soup.find_all("li", class_ = "lx-c-summary-points__item")
   return (item.contents[0] for item in items)
 
-def render(points : Iterable[str], url) -> str:
+bbc = LiveNews("https://www.bbc.com/",
+              r"/news/live/world-europe-.*",
+              bbc_get_points,
+              "BBC's summary:")
+
+# aljazeera = LiveNews("https://www.aljazeera.com/",
+#                     r"https://www.aljazeera.com/news/.*-liveblog",
+#                     undefined,
+#                     "Al Jazeera's summary:")
+
+def find_liveblog_url(live_news : LiveNews) -> str:
+  page = requests.get(live_news.main_url)
+  soup = BeautifulSoup(page.content, "html5lib")
+  links = soup.find_all("a")
+  
+  pat = re.compile(live_news.liveblog_url_pattern)
+  for link in links:
+    url = link["href"]
+    if pat.fullmatch(url):
+      return "https://www.bbc.com" + url
+
+  raise ValueError("No link to a liveblog found on the frontpage.")
+
+def render(points : Iterable[str], header : str, url : str) -> str:
   marked_points = ("- " + point for point in points)
   flatten = "\n\n".join(marked_points)
   wrapped = "\n```\n" + flatten + "\n```"
-  final = wrapped + "\n" + "<" + url +">"
+  final = header + "\n" + wrapped + "\n" + "<" + url +">"
   return final
 
-def get_summary() -> str:
-  url = read_url()
-  soup = get_page(url)
-  url, soup = update_page(url, soup)
-  points = get_points(soup)
-  return render(points, url)
+def get_summary(live_news : LiveNews) -> str:
+  liveblog_url = find_liveblog_url(live_news)
+  points = live_news.get_points(liveblog_url)
+  return render(points, live_news.header, liveblog_url)
 
 if __name__ == "__main__":
-  print(get_summary())
+  print(get_summary(bbc))
